ab420df8f7602c5b3c4a9b46920b0e1a6ac753c1
skripkalisa <skripkalisa.msk@ya.ru>
2022-02-18 21:33:00 +0300

    Mod31 is done

--- Mod31/index.php
+++ Mod31/index.php
@@ -1,5 +1,23 @@
 <?php
-foreach ($reg as $option => $value) {
-    echo $key . ": " . $value . "
-";
+include_once 'HtmlIterator.php';
+
+$file = __DIR__.'/source/code.html';
+$new_file = __DIR__.'/target/code.html';
+
+$html = new HtmlIterator($file);
+
+foreach ($html as $key => $row) {
+    if (str_contains($row, '<title>')) {
+        continue;
+    }
+    if (str_contains($row, '<meta')) {
+        if (str_contains($row, 'keywords') || str_contains($row, 'description')) {
+            continue;
+        }
+    }
+    // print_r($key);
+    // print_r($row);
+    file_put_contents($new_file, $row, FILE_APPEND | LOCK_EX);
 }
+
+echo PHP_EOL;
